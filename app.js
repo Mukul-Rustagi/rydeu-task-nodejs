6658d1e74efdfa828ad5fc68282875850bb167dd
Mukul-Rustagi <adityarustagi1507@gmail.com>
2025-01-04 19:01:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const rideRoutes = require('./routes/rideRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+const Pricing = require('./models/pricing');
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+app.use(errorHandler);
+
+// Routes for ride-related requests
+app.use('/api/ride', rideRoutes);
+
+// Connect to MongoDB and insert pricing data after successful connection
+mongoose.connect('mongodb://localhost:27017/rydeu', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+
+    // Create and insert pricing data for London
+    
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
